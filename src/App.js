01ea3eafc47fffb39378fc904312237debae56fe
Sophie-Ndobe <ndobes76@gmail.com>
2023-11-11 17:01:21 +0200

    Created a Search component and added a form with preliminary css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import Weather from "./Search";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <p>This is where the weather app goes</p>
-        <button className="btn btn-success">Click</button>
+       <Weather />
       </div>
     </div>
   );
