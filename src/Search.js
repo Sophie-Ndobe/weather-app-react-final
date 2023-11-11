25bfa72c56cb8bc8bb3d1529ba1da1a8ecb2477e
Sophie-Ndobe <ndobes76@gmail.com>
2023-11-11 17:19:02 +0200

    Used state management to update the city when searching

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [city, setCity] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(city);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="Search">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Enter a city name"></input>
+            <input
+              type="search"
+              placeholder="Enter a city name"
+              onChange={updateCity}
+            ></input>
           </div>
           <div className="col-3">
             <button className="btn btn-success w-100">Search</button>
